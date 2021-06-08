28a9c20372b7ef49937b52ba0789ed7cdde384c4
CaroBima <82395515+CaroBima@users.noreply.github.com>
2021-06-08 14:00:18 -0300

    Update Producto.java
    creando los constructores, getters y setters

--- src/Logica/Producto.java
+++ src/Logica/Producto.java
@@ -8,6 +8,57 @@ public class Producto {
     private String nombreProducto;
     private String capacidad;
     private String precioProducto;
+
+    public Producto() {
+    }
+
+    public Producto(int idProducto, String tipoProducto, String nombreProducto, String capacidad, String precioProducto) {
+        this.idProducto = idProducto;
+        this.tipoProducto = tipoProducto;
+        this.nombreProducto = nombreProducto;
+        this.capacidad = capacidad;
+        this.precioProducto = precioProducto;
+    }
+
+    public int getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(int idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public String getTipoProducto() {
+        return tipoProducto;
+    }
+
+    public void setTipoProducto(String tipoProducto) {
+        this.tipoProducto = tipoProducto;
+    }
+
+    public String getNombreProducto() {
+        return nombreProducto;
+    }
+
+    public void setNombreProducto(String nombreProducto) {
+        this.nombreProducto = nombreProducto;
+    }
+
+    public String getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(String capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public String getPrecioProducto() {
+        return precioProducto;
+    }
+
+    public void setPrecioProducto(String precioProducto) {
+        this.precioProducto = precioProducto;
+    }
     
     
     
