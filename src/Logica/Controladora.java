0bb694c5085f865f2cabf292a97cb3e09ef5a0b6
CaroBima <82395515+CaroBima@users.noreply.github.com>
2021-06-11 23:21:39 -0300

    Create Controladora.java

---
+++ src/Logica/Controladora.java
@@ -0,0 +1,16 @@
+//para procesar lo que se ingresa en la IGU
+
+package Logica;
+
+public class Controladora {
+    //Agrega un nuevo producto a la base de datos
+    public void agregarProducto(String nombre, String capacidad, int IndiceTipoPord ){
+        
+    }
+    
+    //Agrega un nuevo tipo de producto
+    public void agregarNuevoTipoProducto(String nuevoTipoProducto){
+    
+    }
+    
+}
